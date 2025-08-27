bc60c183141410e22412461a22bf272d58427fd3
us-trigvent <umesh@trigvent.com>
2025-08-27 10:13:09 +0530

    timestamp added

--- models/user.js
+++ models/user.js
@@ -1,48 +1,54 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 4,
-    maxLength: 50,
-  },
-  lastName: {
-    tye: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if (!["male", "female", "others"].includes(value)) {
-        throw new Error("Gender data is not valid");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      tye: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    about: {
+      type: String,
+      maxLength: 500,
+      default: "This user prefers to keep an air of mystery about them.",
+    },
+    skills: {
+      type: [string],
     },
   },
-  photoUrl: {
-    type: String,
-    default:
-      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-  },
-  about: {
-    type: String,
-    maxLength: 500,
-    default: "This user prefers to keep an air of mystery about them.",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("users", userSchema);
 module.exports = User;
