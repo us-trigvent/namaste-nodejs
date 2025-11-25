a8091f35843545fc3a056d67edf0a22769144039
us-trigvent <umesh@trigvent.com>
2025-11-25 16:45:50 +0530

    changes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { userAuth } = require('../middleware/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 const userRouter = express.Router();
 const SAFE_DATA_FIELDS = ['firstName', 'lastName', 'email', 'photoUrl', 'about'];
 userRouter.get('/user/requests/received', userAuth, async (req, res) => {
@@ -42,6 +43,38 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
     } catch (err) {
         res.status(400).send(err.message);
     }
+})
+
+userRouter.get('/feed', userAuth, async (req, res) => {
+    const page = req.query.page || 1
+    let limit = req.query.limit || 10
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    const loggedInUserId = req.user._id;
+    const connectionUserId = await ConnectionRequest.find({
+        $or: [
+            { toUserId: loggedInUserId.toString() },
+            { fromUserId: loggedInUserId.toString() }
+        ]
+    });
+    const hideUsersFromFeed = new Set();
+    connectionUserId.forEach((req) => {
+        hideUsersFromFeed.add(req.fromUserId.toString());
+        hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    const users = await User.find({
+        $and: [
+            { _id: { $nin: Array.from(hideUsersFromFeed) } },
+            { id: { $ne: loggedInUserId } }
+        ]
+
+    }).select(SAFE_DATA_FIELDS).skip(skip).limit(limit)
+    res.send({
+        message: "Users fetched successfully",
+        data: users
+    })
 })
 
 module.exports = userRouter;
